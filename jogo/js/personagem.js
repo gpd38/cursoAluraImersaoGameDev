b6915ae13f774214e32e6e37d7e0cb1817f9dac9
Gustavo Dias <gustavodias2@gmail.com>
2020-06-23 20:44:19 -0300

    Atualizando arquivo personagem.js

--- jogo/js/personagem.js
+++ jogo/js/personagem.js
@@ -1,26 +1,38 @@
 class Personagem{
   constructor(imagem){
     this.imagem=imagem;
-    this.x=0;
-    this.y=0;
+    this.matriz = [
+      [0, 0],
+      [220, 0],
+      [440, 0],
+      [660, 0],
+      [0, 270],
+      [220, 270],
+      [440, 270],
+      [660, 270],
+      [0, 540],
+      [220, 540],
+      [440, 540],
+      [660, 540], 
+      [0, 810],
+      [220, 810],
+      [440, 810],
+      [660, 810],
+    ];
+    this.frameAtual = 0;
   }
   
   exibe(){
-    image(this.imagem, 0, height-135,110,135,this.x,this.y,220,270);
+    image(this.imagem, 0, height - 135,110,135,this.matriz[this.frameAtual][0],this.matriz[this.frameAtual][1],220,270);
     
     this.anima();
   }
   
   anima(){
-    this.x=this.x+220;
+    this.frameAtual = this.frameAtual ++;
     
-    
-    if(this.x>=880){
-      this.x=0;
-      this.y=this.y+270;
-    }
-    if(this.y>=1080){
-      this.y=0;
-    }
+    if(this.frameAtual >= this.matriz.length - 1) {
+      this.frameAtual = 0
+    }    
   }
 }
