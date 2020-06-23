8900edcc80334e66adbe2e43f165cc02437d7bcc
Gustavo Dias <gustavodias2@gmail.com>
2020-06-23 20:43:48 -0300

    Atualizando cenário.js

--- jogo/js/cenario.js
+++ jogo/js/cenario.js
@@ -12,13 +12,13 @@ class Cenario{
   }
   
   move() {
-    this.x1=this.x1-this.velocidade;
-    this.x2=this.x2-this.velocidade;
+    this.x1 = this.x1 - this.velocidade;
+    this.x2 = this.x2 - this.velocidade;
     
-    if(this.x1 <-width){
+    if(this.x1 < -width){
       this.x1=width;
     }
-    if(this.x2 <-width){
+    if(this.x2 < -width){
       this.x2=width;
     }
   } 
