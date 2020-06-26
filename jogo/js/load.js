965add9767bbe66d85eaf81005dee057e96e80ee
Gustavo Dias <gustavodias2@gmail.com>
2020-06-26 14:27:11 -0300

    Adicionando classe de carregamento das imagens do jogo

---
+++ jogo/js/load.js
@@ -0,0 +1,11 @@
+
+function preload() {
+  imagemCenario = loadImage('img/cenario/floresta.png');
+  imagemGameOver = loadImage('img/assets/game-over.png');
+  imagemPersonagem = loadImage('img/personagem/correndo.png');
+  imagemInimigo = loadImage('img/inimigos/gotinha.png');
+  imagemInimigoVoador = loadImage('img/inimigos/gotinha-voadora.png');
+  imagemInimigoGrande = loadImage('img/inimigos/troll.png');
+  somDoJogo = loadSound('som/trilha_jogo.mp3');
+  somDoPulo = loadSound('som/somPulo.mp3');
+}
