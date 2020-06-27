6220161d57a7947c97b22562664008eaf7e1edec
Gustavo Dias <gustavodias2@gmail.com>
2020-06-26 21:31:40 -0300

    Atualizando arquivo jogo.js com as informações da aula5

--- jogo/js/jogo.js
+++ jogo/js/jogo.js
@@ -1,67 +1,78 @@
-// Arquivo de controle dos estados do sistema
-class Jogo{
-	constructor(){
-		this.inimigoAtual = 0;
-	}
+class Jogo {
+  constructor() {
+    this.indice = 0;
+    this.mapa = fita.mapa
+  }
 
-	setup(){
-		cenario = new Cenario(imagemCenario, 3);
-		pontuacao = new Pontuacao();
-		fimDeJogo = false;
+  setup() {
+    fimDeJogo = false;
+    cenario = new Cenario(imagemCenario, 3);
+    pontuacao = new Pontuacao()
+    vida = new Vida(fita.configuracoes.vidaMaxima, fita.configuracoes.vidaInicial)
 
-		personagem = new Personagem(matrizPersonagem, imagemPersonagem, 0, 30, 110, 135, 220, 270);
-		const inimigo = new Inimigo(matrizInimigo, imagemInimigo, width - 52, 30, 52, 52, 104, 104, 10, 100);
-		const inimigoVoador = new Inimigo(matrizInimigoVoador, imagemInimigoVoador, width - 52, 200, 100, 75, 200, 150, 10, 100);
-		const inimigoGrande = new Inimigo(matrizInimigoGrande, imagemInimigoGrande, width , 0, 200, 200, 400, 400, 10, 100);
+    personagem = new Personagem(matrizPersonagem, imagemPersonagem, 0, 30, 110, 135, 220, 270);
+    const inimigo = new Inimigo(matrizInimigo, imagemInimigo, width - 52, 30, 52, 52, 104, 104, 10);
+    const inimigoVoador = new Inimigo(matrizInimigoVoador, imagemInimigoVoador, width - 52, 100, 100, 75, 200, 150, 10);
+    const inimigoGrande = new Inimigo(matrizInimigoGrande, imagemInimigoGrande, width, 0, 200, 200, 400, 400, 15)
 
-		inimigos.push(inimigo);
-		inimigos.push(inimigoGrande);
-		inimigos.push(inimigoVoador);
-	}
+    inimigos.push(inimigo)
+    inimigos.push(inimigoGrande)
+    inimigos.push(inimigoVoador)
+  }
 
-	keyPressed() {
-		if (!fimDeJogo && key === 'ArrowUp') {
-			personagem.pula();
-			somDoPulo.play();
-		}else if(fimDeJogo && key === 'Enter'){
-			window.location.reload();
-		}
-	}
+  keyPressed(key) {
+    if (!fimDeJogo && key === 'ArrowUp') {
+      personagem.pula()
+      somDoPulo.play()
+    }
+    if (fimDeJogo && key === 'Enter') {
+      window.location.reload();
+    }
+  }
 
-	draw() {
-		cenario.exibe();
-		cenario.move();
+  draw() {
+    cenario.exibe();
+    cenario.move();
 
-		pontuacao.exibe();
-		pontuacao.adicionarPonto();
-		personagem.exibe();
-		personagem.aplicaGravidade();
+    vida.draw()
+    pontuacao.exibe()
+    pontuacao.adicionarPonto()
+    personagem.exibe();
+    personagem.aplicaGravidade();
+    const linhaAtual = this.mapa[this.indice]
+    const inimigo = inimigos[linhaAtual.inimigo];
+    const inimigoVisivel = inimigo.x <= -inimigo.largura;
 
-		// Será visualizado um inimigo por vez
-		const inimigo = inimigos[this.inimigoAtual];
-		// Controle do inimigo dentro da tela
-		const inimigoVisivel = inimigo.x < -inimigo.largura;
+    inimigo.velocidade = linhaAtual.velocidade;
 
-		inimigo.exibe();
-		inimigo.move();
+    inimigo.exibe()
+    inimigo.move()
 
-		// Verifica se o inimigo já saiu da tela para que o p´roximo venha
-		if(inimigoVisivel){
-			// Alterar a velocidade dos inimigos para não ficar fácil de mais o jogo
-			this.inimigo.velocidade = parseInt(random(10,30));
-			// Retorna o próximo inimigo
-			this.inimigoAtual++;
-			// Como existe somente 3 inimigos, temos que controlar o restart dos inimigos
-			if(inimigoAtual > 2){
-				this.inimigoAtual = 0;
-			}
-		}
 
-		if (personagem.estaColidindo(inimigo)) {
-			image(imagemGameOver, width/2 - 200, height/3);
+    if (inimigoVisivel) {
+      this.indice++;
+      inimigo.aparece()
 
-			gameOver();
-			noLoop();
-		}    
-	}
+      if (this.indice > this.mapa.length - 1) {
+        this.indice = 0;
+      }
+    }
+
+    if (personagem.estaColidindo(inimigo)) {
+      vida.perdeVida();
+      personagem.tornarInvencivel()
+      if (vida.vidas === 0) {
+        image(imagemGameOver, width / 2 - 200, height / 3);
+        fill("#fff");
+        somDoJogo.stop()
+
+        textAlign(CENTER)
+        textSize(30);
+        text("Pressione ENTER para tentar novamente.", width / 2, height / 5 * 3)
+        fimDeJogo = true;
+        noLoop();
+      }
+    }
+  }
+
 }
