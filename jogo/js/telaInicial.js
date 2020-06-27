a5b532c3a4e1c31add46e25187655f55715d153d
Gustavo Dias <gustavodias2@gmail.com>
2020-06-26 21:33:46 -0300

    Atualizando arquivo telaInicial.js com as informações da aula5

--- jogo/js/telaInicial.js
+++ jogo/js/telaInicial.js
@@ -1,10 +1,32 @@
-class TelaInicial{
-	constructor(){
+class TelaInicial {
+  constructor() {
 
-	}
+  }
 
-	draw(){
-		console.log('telaInicial');
-	}
+  draw() {
+    this._imagemDeFundo();
+    this._texto();
+    this._botaoStart();
+    // this._botaoHistoria();
+  }
+
+  _imagemDeFundo() {
+    // Exibir a tela inicial do jogo onde o start aparece
+    image(imagemTelaInicial, 0, 0, width, height);
+  }
+
+  _texto() {
+    textFont(fonteTelaInicial);
+    textAlign(CENTER)
+    textSize(50);
+    text('As aventuras de', width / 2, height / 3);
+    textSize(150);
+    text('Hipsta', width / 2, height / 5 * 3)
+  }
+
+  _botaoStart() {
+    botaoGerenciador.y = height / 7 * 5;
+    botaoGerenciador.draw();
+  }
 
 }
